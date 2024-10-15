7aaa3252e101656680e530a6a513959b02a2f4ee
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-15 11:14:23 +0900

    feat: 더미데이터로 무한 스크롤 구현

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,11 +1,18 @@
 import axios from "axios";
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 import BackHeader from "@/components/BackHeader";
 import DocumentItem from "@/components/DocumentItem";
 import Footer from "@/components/Footer";
+
+interface UseObserverProps {
+  target: MutableRefObject<Element | null>; // Ref 객체로 전달되는 DOM 요소
+  rootMargin?: string; // rootMargin의 기본값은 "0px"
+  threshold?: number | number[]; // threshold는 0~1 사이의 숫자 또는 숫자 배열
+  onIntersect: IntersectionObserverCallback; // IntersectionObserver 콜백 타입
+}
 
 export default function HistoryPage() {
   const apiUrl = "https://jsonplaceholder.typicode.com";
@@ -32,6 +39,39 @@ export default function HistoryPage() {
       },
     });
 
+  const useObserver = ({
+    target,
+    rootMargin = "0px",
+    threshold = 1.0,
+    onIntersect,
+  }: UseObserverProps) => {
+    useEffect(() => {
+      let observer: IntersectionObserver | undefined;
+
+      if (target && target.current) {
+        observer = new IntersectionObserver(onIntersect, {
+          root: null,
+          rootMargin,
+          threshold,
+        });
+
+        observer.observe(target.current);
+      }
+      return () => observer && observer.disconnect();
+    }, [target, rootMargin, threshold, onIntersect]);
+  };
+
+  const onIntersect = ([entry]: IntersectionObserverEntry[]) => {
+    entry.isIntersecting && fetchNextPage();
+  };
+
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useObserver({
+    target: bottomRef,
+    onIntersect,
+  });
+
   return (
     <>
       <BackHeader />
@@ -44,18 +84,7 @@ export default function HistoryPage() {
               ))}
             </React.Fragment>
           ))}
-          <div>
-            <button
-              onClick={() => fetchNextPage()}
-              disabled={!hasNextPage || isFetchingNextPage}
-            >
-              {isFetchingNextPage
-                ? "로딩 중.."
-                : hasNextPage
-                ? "불러오기"
-                : "데이터 끝"}
-            </button>
-          </div>
+          <div ref={bottomRef}></div>
         </div>
       </div>
       <Footer />
